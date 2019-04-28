6097a555fd65e2fa1baf2abb01f4dea36eb6d730
missing <whereismissing@gmail.com>
2019-04-28 16:04:01 -0500

    Init search.php PDO LIKE query example

---
+++ html/search.php
@@ -0,0 +1,26 @@
+<?php
+
+require("db_connect.php");
+
+$search = $_POST['search'];
+
+$search = "%$search%";
+
+$sql = "SELECT * FROM items WHERE author LIKE :author";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->bindValue(':author', $search);
+
+$stmt->execute([$search]);
+
+$sqlRow = $stmt->fetchAll();
+
+print_r($sqlRow);
+
+/*
+for ($i = 0; $i < count($sqlRow); $i++) {
+    echo $sqlRow[$i] . "\n";
+}*/
+
+?>
