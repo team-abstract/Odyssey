3a23d7145984cc33f59e4b5149c3f472a6d95ce0
missing <whereismissing@gmail.com>
2019-04-28 16:26:02 -0500

    Update cardowner.php

--- html/cardowner.php
+++ html/cardowner.php
@@ -1,23 +1,5 @@
 <?php 
-    session_start();
-    function isLoggedIn() {
-	if (isset($_SESSION['user'])) {
-		return true;
-	} else {
-		return false;
-	}
-    }
-
-    if (!isLoggedIn()) {
-	$_SESSION['msg'] = "You must log in first";
-	header('location: index.html');
-    }
-
-    if (isset($_GET['logout'])) {
-	session_destroy();
-	unset($_SESSION['user']);
-	header("location: index.html");
-    }
+include('auth.php');
 ?>
 <!DOCTYPE html>
 <html>
