402f494a841365df05f57263a8263bca8cc51630
missing <whereismissing@gmail.com>
2019-04-29 17:45:14 -0500

    Added server logic to display cardowner values

--- html/cardowner.php
+++ html/cardowner.php
@@ -1,5 +1,6 @@
 <?php 
 include('auth.php');
+require('db_connect.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -40,7 +41,7 @@ include('auth.php');
         <h1><center> <img src="./odyssey_logo.png" style="height:100px;"> </center></h1>
         <div class="search-bar">
             <form action="/search.php" method="post">
-                <input type="text" placeholder="Search for a title" name="search">
+                <input type="text" placeholder="Search for a title" name="title">
                 <button type="submit"><i class="fa fa-search"></i></button>
             <button onclick="showadv();" type="button">Advanced Search</button>
             <br>
@@ -54,7 +55,30 @@ include('auth.php');
             </div>
             </form>
         </div>
+
+        <?php
+	    echo $_SESSION['user'] . "<br>";
+	    //echo $_SESSION['userid'];
+	    $userq = $_SESSION['userid'];
+	    $query = "SELECT itemOnHoldID, outstandingFees FROM cardOwners WHERE userID = :user";
+	    $dbobj = $pdo->prepare($query);
+	    $dbobj->bindValue(':user', $userq);
+	    $dbobj->execute();
+	    // view books on hold, checkedout, fees
+	    // TODO: pretty print in HTML
+	    $sqlarray = $dbobj->fetch(PDO::FETCH_ASSOC);
+	    print("items on hold: " . $sqlarray[itemOnHoldID] . "<br>");
+	    print("outstanding fees: " . $sqlarray[outstandingFees] . "<br>");
+
+            $chkBooksQuery = "SELECT title FROM items WHERE bibnum=(SELECT bibnum FROM checkedBooks WHERE cardOwnerID=(SELECT cardOwnerID FROM cardOwners WHERE userID=:user))";
+	    $chkBooksDBobj = $pdo->prepare($chkBooksQuery);
+            $chkBooksDBobj->bindValue(':user', $userq);
+            $chkBooksDBobj->execute();
+            print("checked books: ");
+            print_r($chkBooksDBobj->fetchAll());
+        ?>
         </body>
+
     </div>
 </body>
 </html>
