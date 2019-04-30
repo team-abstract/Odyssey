daa917145202783354f8281ff3e987f6c8937813
missing <whereismissing@gmail.com>
2019-04-30 17:17:57 -0500

    Cleaned up cardowner.php styling

--- html/cardowner.php
+++ html/cardowner.php
@@ -1,61 +1,131 @@
-<?php
-
-    include('auth.php');
-
-    require("db_connect.php");
-
-    print_r($qoutput);
-
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $phone = $_POST['phone'];
-
-    $fname = "%$fname%";
-    $lname = "%$lname%";
-    $phone = "%$phone%";
-
-    $query = "SELECT * FROM cardOwners WHERE userid=(SELECT id FROM users WHERE firstName LIKE :fname AND lastName LIKE :lname AND phoneNum LIKE :phone )";
-
-    $dbobj = $pdo->prepare($query);
-
-    $dbobj->bindValue(':fname', $fname);
-    $dbobj->bindValue(':lname', $lname);
-    $dbobj->bindValue(':phone', $phone, PDO::PARAM_INT);
-
-    $dbobj->execute();
-
-    $qoutput = $dbobj->fetchAll();
-    print_r($qoutput);
-
-    $cardOwnerID = $qoutput[0][cardOwnerID];
-    $isSuspended = $qoutput[0][isSuspended];
-    $userID = $qoutput[0][userID];
-
+<?php 
+include('auth.php');
+require('db_connect.php');
 ?>
-
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Odyssey Library Homepage</title>
+    <link rel="stylesheet" type="text/css" href="tmpstyles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script>
+        function showadv() {
+            // toggle visibility of advanced search elements
+            if (document.getElementById("advoptions").style.visibility == "visible") {
+                console.log("[+] hiding options");
+                document.getElementById("advoptions").style.visibility = "hidden";
+            } else {
+                console.log("[+] making visible");
+                document.getElementById("advoptions").style.visibility = "visible";
+            }
+        }
+    </script>
+
+    <style>
+	#userinfo {
+	  margin-bottom: 15px;
+	  padding: 4px 12px;
+	}
+
+	.danger {
+	  background-color: #ffdddd;
+	  border-left: 6px solid #f44336;
+	}
+
+	.success {
+	  background-color: #ddffdd;
+	  border-left: 6px solid #4CAF50;
+	}
+
+	.info {
+	  background-color: #e7f3fe;
+	  border-left: 6px solid #2196F3;
+	}
+
+	.warning {
+	  background-color: #ffffcc;
+	  border-left: 6px solid #ffeb3b;
+	}
+    </style>
+</head>
 <body>
-    <form action="updateCardOwner.php" method="post">
-        <input type="text" placeholder="Email" name="email">
-        <input type="text" placeholder="First Name" name="fname">
-        <input type="text" placeholder="Last Name" name="lname">
-        <input type="text" placeholder="Phone Number" name="phoneNum">
-        <input type="hidden" name="userID" value=<?php print($userID); ?>>
-        <button type="submit">Update Card Owner Info</button>
-    </form>
+    <div class="header">
+        <h2>Search the Odyssey Library Collection</h2>
+    </div>
+    <div class="content">
+        <!-- logged in user information -->
+        <body>
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a> Cardowner </a>
+            <a> <?php echo $_SESSION['user'] ?> </a>
+            <div class="login-container">
+                    <form action="cardowner.php" method="get">
+                        <button type="submit" name="logout" value="1">Log Out</button>
+                    </form>
+                </div>
+        </div>
+        <br><br><br><br>
+        <!--<h1><center>Welcome, <?php $name = $_SESSION['firstName']; echo $name; ?>!</center></h1>-->
+        <br><br><br><br>
+        <h1><center> <img src="./odyssey_logo.png" style="height:100px;"> </center></h1>
+        <div class="search-bar">
+            <form action="/search.php" method="post">
+                <input type="text" placeholder="Search for a title" name="title">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            <button onclick="showadv();" type="button">Advanced Search</button>
+            <br>
+            <div id="advoptions">
+		    <input type="text" placeholder="Author" name="author">
+		    <input type="text" placeholder="ISBN" name="isbn">
+		    <input type="text" placeholder="Publication Year" name="pubyear">
+		    <input type="text" placeholder="Publisher" name="publisher">
+		    <input type="text" placeholder="Item Type" name="itemtype">
+		    <input type="text" placeholder="Genre" name="genre">
+            </div>
+            </form>
+        </div>
 
-    <form action="switchCardOwnerStatus.php" method="get">
-        <input type="hidden" name="cardOwnerID" value=<?php print($qoutput[0][cardOwnerID]); ?> >
-        <input type="hidden" name="isSuspended" value=<?php print($isSuspended); ?> >
-        <button type="submit">Toggle Card Owner Status </button>
-    </form>
+        <?php
+	    echo "<div id=\"userinfo\" class=\"success\">" . $_SESSION['user'] . "<br>" . "</div>";
 
-    <form action="checkOutBook.php" method="post">
-        <input type="text" placeholder="cardOwnerID" name="cardOwnerID" value=<?php print($cardOwnerID); ?>>
-        <input type="text" placeholder="bibnum" name="bibnum">
-        <button type="submit">Check out a book</button>
-    </form>
+	    $userq = $_SESSION['userid'];
+	    $query = "SELECT itemOnHoldID, outstandingFees FROM cardOwners WHERE userID = :user";
+	    $dbobj = $pdo->prepare($query);
+	    $dbobj->bindValue(':user', $userq);
+	    $dbobj->execute();
+	    // view books on hold, checkedout, fees
+	    // TODO: pretty print in HTML
+	    $sqlarray = $dbobj->fetch(PDO::FETCH_ASSOC);
 
+	    echo "<div id=\"userinfo\" class=\"info\">" . "Items On Hold: " . $sqlarray[itemOnHoldID] . "<br>" . "</div>";
+
+            if ($sqlarray[outstandingFees] === 0) {
+                echo "<div id=\"userinfo\" class=\"info\">";
+            } else {
+                echo "<div id=\"userinfo\" class=\"danger\">";
+            }
+
+            echo "Outstanding Fees: " . $sqlarray[outstandingFees] . "<br>" . "</div>";
+            
+            $chkBooksQuery = "select title from items, checkedBooks where items.bibNum = checkedBooks.bibNum AND checkedBooks.cardOwnerID=:user";
+	    $chkBooksDBobj = $pdo->prepare($chkBooksQuery);
+            $chkBooksDBobj->bindValue(':user', $userq);
+            $chkBooksDBobj->execute();
+
+            echo "<div id=\"userinfo\" class=\"warning\">" . "Checked Books: <br></div>";
+
+            echo "<ul>";
+	    foreach($chkBooksDBobj as $chkB){ 
+		//echo "&emsp;" . "&emsp;" .  $chkB['title']; 
+		//echo "<br>";
+                echo "<li>" . $chkB['title'] . "</li>";
+	    }
+            echo "</ul>";
+            //print_r($chkBooksDBobj->fetchAll());
+            ?>
+        </body>
+
+    </div>
 </body>
 </html>
