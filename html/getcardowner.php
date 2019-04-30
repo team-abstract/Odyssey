6d04088e70e11e02cace26fc27bc141cb04a57bb
missing <whereismissing@gmail.com>
2019-04-29 20:50:29 -0500

    Added html for user input

--- html/getcardowner.php
+++ html/getcardowner.php
@@ -1,28 +1,60 @@
 <?php
 
-include('auth.php');
+    include('auth.php');
 
-require("db_connect.php");
+    require("db_connect.php");
 
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$phone = $_POST['phone'];
+    print_r($qoutput);
 
-$fname = "%$fname%";
-$lname = "%$lname%";
-$phone = "%$phone%";
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $phone = $_POST['phone'];
 
-$query = "SELECT * FROM cardOwners WHERE userid=(SELECT id FROM users WHERE firstName LIKE :fname AND lastName LIKE :lname AND phoneNum LIKE :phone )";
+    $fname = "%$fname%";
+    $lname = "%$lname%";
+    $phone = "%$phone%";
 
-$dbobj = $pdo->prepare($query);
+    $query = "SELECT * FROM cardOwners WHERE userid=(SELECT id FROM users WHERE firstName LIKE :fname AND lastName LIKE :lname AND phoneNum LIKE :phone )";
 
-$dbobj->bindValue(':fname', $fname);
-$dbobj->bindValue(':lname', $lname);
-$dbobj->bindValue(':phone', $phone, PDO::PARAM_INT);
+    $dbobj = $pdo->prepare($query);
 
-$dbobj->execute();
+    $dbobj->bindValue(':fname', $fname);
+    $dbobj->bindValue(':lname', $lname);
+    $dbobj->bindValue(':phone', $phone, PDO::PARAM_INT);
 
-$qoutput = $dbobj->fetchAll();
-print_r($qoutput);
+    $dbobj->execute();
+
+    $qoutput = $dbobj->fetchAll();
+    print_r($qoutput);
+
+    $cardOwnerID = $qoutput[0][cardOwnerID];
+    $isSuspended = $qoutput[0][isSuspended];
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<body>
+    <form action="updateCardOwner.php" method="post">
+        <input type="text" placeholder="Email" name="email">
+        <input type="text" placeholder="First Name" name="fname">
+        <input type="text" placeholder="Last Name" name="lname">
+        <input type="text" placeholder="Phone Number" name="phoneNum">
+        <input type="hidden" name="cardOwnerID" value=<?php print($cardOwnerID); ?>>
+        <button type="submit">Update Card Owner Info</button>
+    </form>
+
+    <form action="switchCardOwnerStatus.php" method="get">
+        <input type="hidden" name="cardOwnerID" value=<?php print($qoutput[0][cardOwnerID]); ?> >
+        <input type="hidden" name="isSuspended" value=<?php print($isSuspended); ?> >
+        <button type="submit">Toggle Card Owner Status </button>
+    </form>
+
+    <form action="checkOutBook.php" method="post">
+        <input type="text" placeholder="cardOwnerID" name="cardOwnerID" value=<?php print($cardOwnerID); ?>>
+        <input type="text" placeholder="bibnum" name="bibnum">
+        <button type="submit">Check out a book</button>
+    </form>
+
+</body>
+</html>
